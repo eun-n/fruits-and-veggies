dbf17da2591054b4accead05da910e23082e5011
eun-n <eunicena.12@gmail.com>
2016-05-23 15:29:21 -0700

    basic sorting done git push

--- starter-code/js/app.js
+++ starter-code/js/app.js
@@ -4,3 +4,48 @@
 console.log('App Started');
 console.log('Fruit count', fruit.length);
 console.log('Veggie count', vegetables.length);
+
+var fruitsandvegs = angular.module('FruitsnVegs', []);
+
+fruitsandvegs.controller('HomeCtrl', ['$scope', function($scope) {
+
+$scope.fruits = [
+  'Apple',
+  'Apricot',
+  'Banana'
+  ];
+
+$scope.vegetables = [
+  'Artichoke',
+  'Arugula',
+  'Asparagus'
+  ];
+
+$scope.show = true;
+
+$scope.middle = $scope.fruits.concat($scope.vegetables);
+
+	$scope.fruity = [];
+
+$scope.makeFruit = function(a) {
+		$scope.fruity.push(a);
+		for(var i=0; i<$scope.middle.length; i++) {
+			if($scope.middle[i]===a) {
+				$scope.middle.splice(i, 1);
+			}
+		}
+  		a= '';
+	}
+
+	$scope.vegy = [];
+
+	$scope.makeVeg = function(b) {
+		$scope.vegy.push(b);
+		for(var i=0; i<$scope.middle.length; i++) {
+			if($scope.middle[i]===b) {
+				$scope.middle.splice(i, 1);
+			}
+		}
+  		b= '';
+	}
+  }]);
